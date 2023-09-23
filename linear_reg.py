9ba9769432cc01e6bee308a06b02a5daebe2e612
TimKang13 <70048353+TimKang13@users.noreply.github.com>
2023-09-23 00:56:24 -0400

    linear

--- linear_reg.py
+++ linear_reg.py
@@ -19,11 +19,13 @@ l_reg = linear_model.LinearRegression()
 
 #visualize with pyplot of matplotlib
 plt.scatter(X.T[1], y)
-plt.show()
+plt.show
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = l_reg.fit(X_train, y_train)
 predictions = model.predict(X_test)
 print("Predictions: ", predictions)
-print("R^2 value: " l_reg.score(X,y))
+print("R^2 value: ", l_reg.score(X,y))
+print("coedd: ", l_reg.coef_)
+print("intercept: ", l_reg.intercept_)
