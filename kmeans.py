528f26098921fff4d66a8c94e94086ed6d30adf5
TimKang13 <70048353+TimKang13@users.noreply.github.com>
2023-09-23 11:22:26 -0400

    kmeans and digits recognizer

---
+++ kmeans.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import scale
+import pandas as pd
+
+bc = load_breast_cancer()
+print(bc)
+
+#x = bc.data
+# very big or small, so let's scale
+X = scale(bc.data)
+y = bc.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = KMeans(n_clusters=2, random_state = 0)
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+labels = model.labels_
+print('labels: ', labels)
+print('predictions: ', predictions)
+
+print('accuracy: ', accuracy_score(y_test, predictions))
+print('actual: ', y_test)
+
+print(pd.crosstab(y_train, labels))  #flips if labels are incorrect
+
+
+
